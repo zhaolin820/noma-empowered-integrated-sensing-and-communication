77781a7541c2b5df2a3ebca9ec4af59c147a4582
Zhaolin Wang <56995193+zhaolin820@users.noreply.github.com>
2022-06-20 09:35:25 +0100

    Update SCA_algorithm.m

--- function/SCA_algorithm.m
+++ function/SCA_algorithm.m
@@ -17,15 +17,14 @@ epsilon_inner = 1.001;
 epsilon_outter = 1e-4;
 eta = 1e5;
 
-
 H = zeros(para.N, para.N, para.K);
 for k = 1:para.K
     h_k = h(:,k);
     H(:,:,k) = h_k * h_k';
 end
 
-[W_n, r_n, R_pre] = initial_point(para, H);
-if W_n == 0
+[W_n,cvx_status] = initial_point(para, H);
+if strcmp(cvx_status, 'Infeasible')
     W = 0; R_curr = 0; P = 0;
     return;
 end
@@ -38,12 +37,10 @@ for k = 1:para.K
     s = svd(W_n(:,:,k));
     rank_diff = rank_diff + sum(s) - max(s);
 end
-rank_diff_all = [];
-R_all =[];
-P_all = [];
-R_all = [R_all, sum(min(R_curr,[],2))];
-P_all = [P_all, sum(P)];
-rank_diff_all = [rank_diff_all, rank_diff];
+
+R_all = [sum(min(R_curr,[],2))];
+P_all = [sum(P)];
+rank_diff_all = [rank_diff];
 
 %% SCA algorithm
 rank_diff = 100;
@@ -51,48 +48,10 @@ f_pre = -1;
 while rank_diff > epsilon_outter
     f_frac = 100;
 	while f_frac > epsilon_inner
-        cvx_begin quiet
 
-            variable W(para.N,para.N,para.K) complex
-            variable r(para.K, 1)
-            Rw = sum(W,3);
-            [P] = probing_power(para, Rw);
-
-            [sca] = SCA_SINR(para, H, W, W_n, r);
-
-            [corr] = cross_correlation(para, Rw);
-            
-            % constraints
-            for k = 1:para.K
-                W(:,:,k) == hermitian_semidefinite(para.N);
-                r(k) >= para.R_min;
-            end
-
-            for i = 1:length(sca)
-                sca(i) >= 0;
-            end
-            M = length(para.theta);
-            for k = 1:M
-                for p = k+1:M
-                    P(k) - P(p) <= 10;
-                    P(k) - P(p) >= -10;
-                end
-            end
-
-            diag(Rw) == para.Pt / para.N;
-            corr <= para.correlation_min;
-            
-            % objective function
-            [f] = obj_func(W, W_n, r, P, para.K, rho_c, rho_r, eta);
-            minimize (f);
-        cvx_end
+        [W,f,P,cvx_status] = SCA_step(para, W_n, H, rho_c, rho_r, eta);
         
-        if strcmp(cvx_status, 'Infeasible')
-            f_frac = 1;
-            W = W_n; r = r_n;
-            Rw = sum(W,3);
-            [P] = probing_power(para, Rw);
-        elseif strcmp(cvx_status, 'Failed')
+        if strcmp(cvx_status, 'Failed') || strcmp(cvx_status, 'Infeasible')
             W = 0; R_curr = 0; P = 0;
             return;
         else
@@ -100,7 +59,7 @@ while rank_diff > epsilon_outter
             disp(['sum rate -- ', num2str(sum(min(R_curr,[],2))),' / sum probing power-- ', num2str(sum(P)) ' / objective value', num2str(f)]);
             f_frac = min(f, f_pre) / max(f, f_pre);
             f_pre = f;
-            W_n = W; r_n = r;
+            W_n = W;
         end
     end
     
@@ -120,6 +79,47 @@ while rank_diff > epsilon_outter
 end
 [R_curr] = rate_NOMA(para, W, H);
 end
+
+
+function [W,f,P,cvx_status] = SCA_step(para, W_n, H, rho_c, rho_r, eta)
+    cvx_begin quiet
+    
+        variable W(para.N,para.N,para.K) complex
+        variable r(para.K, 1)
+        Rw = sum(W,3);
+        [P] = probing_power(para, Rw);
+    
+        [sca] = SCA_SINR(para, H, W, W_n, r);
+    
+        [corr] = cross_correlation(para, Rw);
+        
+        % constraints
+        for k = 1:para.K
+            W(:,:,k) == hermitian_semidefinite(para.N);
+            r(k) >= para.R_min;
+        end
+    
+        for i = 1:length(sca)
+            sca(i) >= 0;
+        end
+        M = length(para.theta);
+        for k = 1:M
+            for p = k+1:M
+                P(k) - P(p) <= 10;
+                P(k) - P(p) >= -10;
+            end
+        end
+    
+        diag(Rw) == para.Pt / para.N;
+        corr <= para.correlation_min;
+        
+        % objective function
+        [f] = obj_func(W, W_n, r, P, para.K, rho_c, rho_r, eta);
+        minimize (f);
+    cvx_end
+end
+
+
 
 %% objective function
 function [f] = obj_func(W, W_n, r, P, K, rho_c, rho_r, eta)
@@ -142,7 +142,7 @@ function [p] = rank_penalty(W, W_n, K)
 end
 
 %% initialize
-function [W, r, R_initial] = initial_point(para, H)
+function [W,cvx_status] = initial_point(para, H)
 
     cvx_begin quiet
         
@@ -170,12 +170,5 @@ function [W, r, R_initial] = initial_point(para, H)
 
         diag(Rw) == para.Pt / para.N;
         corr <= para.correlation_min;
-    cvx_end
-    
-    [R_initial] = rate_NOMA(para, W, H);
-    r = 2^para.R_min * ones(para.K,1);
-    if strcmp(cvx_status, 'Infeasible')
-        W = 0; r = 0; R_initial = 0;
-    end
-    
+    cvx_end   
 end
