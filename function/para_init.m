7738fd1f67ea7844947675b7b45c1e081d4ca5f2
Zhaolin Wang <56995193+zhaolin820@users.noreply.github.com>
2022-06-20 09:35:48 +0100

    Update para_init.m

--- function/para_init.m
+++ function/para_init.m
@@ -14,7 +14,7 @@ values.N = 4; % overall antennas
 
 values.Pt = 10^(20/10); % overall transmit power
 values.n = 1; % equivalent noise power
-values.K = 6; % user number
+values.K = 3; % user number
 values.theta = [-40, 40]*pi/180; % desired directions
 values.weight = ones(values.K,1); % weight of weighted sum rate 
 values.pathloss_direct =  @(d) 32.6 + 36.7*log10(d); % path loss with d in m
@@ -25,7 +25,6 @@ values.user_range = [50, 200]; % range of user location from RIS in m
 
 values.t = 0; % channel correlation factor
 values.R_min = 1; % minimum rate
-values.P_min = 0; % minimum probing power
 values.correlation_min = 10; % minimum cross-correlation
 
 end
